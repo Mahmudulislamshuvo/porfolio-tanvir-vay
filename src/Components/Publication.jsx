404e618d074658f9253bef295841d17a5c516481
Mahmudul Islam <mahmudulislammern@gmail.com>
2025-06-21 12:32:52 +0600

    Publication fixed

--- src/Components/Publication.jsx
+++ src/Components/Publication.jsx
@@ -39,83 +39,87 @@ const Publication = () => {
   ];
 
   return (
-    <div className="container">
-      <div className="bg-[#FCFCFC] py-10">
-        <div className="flex items-center gap-x-2">
-          <span className="h-2 w-2 rounded-full bg-primary"></span>
-          <h2 className="text-5xl py-10">Publications</h2>
-        </div>
-        <div className="flex flex-wrap justify-between gap-y-5">
-          {publications.map((publication) => (
-            <div className="group relative overflow-hidden bg-gradient-to-br from-white to-gray-50 border border-gray-100 rounded-2xl shadow-lg transition-all duration-300 hover:shadow-xl p-6 max-w-2xl w-[49%]">
-              {/* Decorative elements */}
-              <div className="absolute top-0 right-0 w-24 h-24">
-                <div className="absolute top-0 right-0 w-12 h-12 border-t-2 border-r-2 border-blue-400 rounded-tr-2xl"></div>
-              </div>
-
-              <div className="absolute bottom-0 left-0 w-16 h-16 opacity-10 group-hover:opacity-30 transition-opacity">
-                <div className="absolute bottom-0 left-0 w-8 h-8 border-b-2 border-l-2 border-purple-400 rounded-bl-2xl"></div>
-              </div>
-
-              {/* Content */}
-              <div className="relative z-10">
-                {/* Category badge */}
-                <span className="inline-block px-3 py-1 text-xs font-semibold text-blue-600 bg-blue-100 rounded-full mb-4">
-                  {publication.type}
-                </span>
-
-                {/* Title */}
-                <h1 className="text-xl md:text-2xl font-bold text-gray-800 mb-3  transition-colors">
-                  {publication.title}
-                </h1>
-
-                {/* Journal info */}
-                <div className="flex items-center mb-2">
-                  <EducationPlug />
-                  <p className="text-md font-medium text-gray-700">
-                    {publication.journal}
-                  </p>
+    <div className="bg-[#FCFCFC]">
+      <div className="container">
+        <div className="py-10">
+          <div className="flex items-center justify-center">
+            <div className="flex items-center gap-x-2">
+              <span className="h-2 w-2 rounded-full bg-primary"></span>
+              <h2 className="text-5xl py-10 text-center">Publications</h2>
+            </div>
+          </div>
+          <div className="flex flex-wrap justify-between gap-y-5">
+            {publications.map((publication) => (
+              <div className="group relative overflow-hidden bg-gradient-to-br from-white to-gray-50 border border-gray-100 rounded-2xl shadow-lg transition-all duration-300 hover:shadow-xl p-6 max-w-2xl w-[49%]">
+                {/* Decorative elements */}
+                <div className="absolute top-0 right-0 w-24 h-24">
+                  <div className="absolute top-0 right-0 w-12 h-12 border-t-2 border-r-2 border-blue-400 rounded-tr-2xl"></div>
                 </div>
 
-                <div className="flex flex-wrap items-center gap-4 mt-4 pt-4 border-t border-gray-100">
-                  {/* Year */}
-                  <div className="flex items-center">
-                    <CalenderICon />
-                    <p className="text-sm font-semibold text-gray-600">
-                      {" "}
-                      {publication.year}
+                <div className="absolute bottom-0 left-0 w-16 h-16 opacity-10 group-hover:opacity-30 transition-opacity">
+                  <div className="absolute bottom-0 left-0 w-8 h-8 border-b-2 border-l-2 border-purple-400 rounded-bl-2xl"></div>
+                </div>
+
+                {/* Content */}
+                <div className="relative z-10">
+                  {/* Category badge */}
+                  <span className="inline-block px-3 py-1 text-xs font-semibold text-blue-600 bg-blue-100 rounded-full mb-4">
+                    {publication.type}
+                  </span>
+
+                  {/* Title */}
+                  <h1 className="text-xl md:text-2xl font-bold text-gray-800 mb-3  transition-colors">
+                    {publication.title}
+                  </h1>
+
+                  {/* Journal info */}
+                  <div className="flex items-center mb-2">
+                    <EducationPlug />
+                    <p className="text-md font-medium text-gray-700">
+                      {publication.journal}
                     </p>
                   </div>
 
-                  {/* Publication Type */}
-                  <div className="flex items-center">
-                    <RightSignIcon />
-                    <p className="text-sm font-semibold text-gray-600">
-                      {publication.publicationType}
-                    </p>
+                  <div className="flex flex-wrap items-center gap-4 mt-4 pt-4 border-t border-gray-100">
+                    {/* Year */}
+                    <div className="flex items-center">
+                      <CalenderICon />
+                      <p className="text-sm font-semibold text-gray-600">
+                        {" "}
+                        {publication.year}
+                      </p>
+                    </div>
+
+                    {/* Publication Type */}
+                    <div className="flex items-center">
+                      <RightSignIcon />
+                      <p className="text-sm font-semibold text-gray-600">
+                        {publication.publicationType}
+                      </p>
+                    </div>
+
+                    {/* DOI */}
+                    <div className="flex items-center">
+                      <LinkIcon />
+                      <a
+                        href="https://doi.org/10.1007/978-981-97-3966-0_8"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-sm font-medium text-blue-600 hover:text-blue-800 hover:underline transition-colors"
+                      >
+                        {publication.doi}
+                      </a>
+                    </div>
                   </div>
 
-                  {/* DOI */}
-                  <div className="flex items-center">
-                    <LinkIcon />
-                    <a
-                      href="https://doi.org/10.1007/978-981-97-3966-0_8"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="text-sm font-medium text-blue-600 hover:text-blue-800 hover:underline transition-colors"
-                    >
-                      {publication.doi}
-                    </a>
-                  </div>
-                </div>
-
-                {/* Abstract button */}
-                {/* <button className="mt-6 px-4 py-2 text-sm font-medium text-white bg-gradient-to-r from-blue-500 to-purple-500 rounded-lg transition-all duration-300 transform group-hover:-translate-y-0.5 hover:shadow-md">
+                  {/* Abstract button */}
+                  {/* <button className="mt-6 px-4 py-2 text-sm font-medium text-white bg-gradient-to-r from-blue-500 to-purple-500 rounded-lg transition-all duration-300 transform group-hover:-translate-y-0.5 hover:shadow-md">
                      View Abstract
                    </button> */}
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
     </div>
